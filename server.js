9e24b1289253019a45ac0bfb93a5db6a23cd37a0
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-10-27 20:21:14 -0700

    troubleshooting heroku connection via server.js

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ const PORT = process.env.PORT || 3001
 
 const hbs = exphbs.create({ helpers })
 
-const sess= {
+const sess = {
     secret: process.env.SESSION_SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize
-    })
+        db: sequelize,
+    }),
 }
 
 app.use(session(sess))
@@ -35,5 +35,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
+    app.listen(PORT, () => console.log('Now listening'))
 })
